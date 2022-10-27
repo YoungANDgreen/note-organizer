363fb0114cf903811ee7784c16b673e4c2e45f60
Ryan Younggreen <ryounggreen@gmail.com>
2022-10-26 22:21:24 -0600

    Initial Commit

---
+++ Develop/routes/notes.js
@@ -0,0 +1,32 @@
+const note = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+
+note.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+
+note.post('/', (req, res) => {
+  
+  const { title, text } = req.body;
+
+  
+  if (req.body) {
+    
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = note;
