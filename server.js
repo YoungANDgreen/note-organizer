afba2b251adeac77dff570287de5beb26c44e782
Ryan Younggreen <ryounggreen@gmail.com>
2022-11-02 22:57:21 -0600

    trying

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./Develop/public/assets/js/index');
+const api = require('./assets/js/index');
 
 const PORT = process.env.PORT || 3001;
 
